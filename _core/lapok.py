9ec416885a1130765fdf715fd2ff8390bf2bde27
kobanya <nagy.bela.budapest@gmail.com>
2023-10-14 23:17:33 +0200

    még vannak hibák

--- _core/lapok.py
+++ _core/lapok.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Kezdolap.models import Nyilvantartas
 
-def kezdolap(lekeres):
+def kezdolap(request):
     nyilvantartas = Nyilvantartas.objects.all()
+    return render(request, 'index.html', {"nyilvantartas": nyilvantartas})
 
-    return render(lekeres, 'templates/index.html',{"nyilvantartas":nyilvantartas})
+def uj_adat(request):
+    if request.method == "POST":
+        vezetek_nev = request.POST.get('vezetek_nev')
+        kereszt_nev = request.POST.get('kereszt_nev')
+        kulcs_szam = request.POST.get('kulcs_szam')
 
+        nyilvantartas = Nyilvantartas(vezetek_nev=vezetek_nev, kereszt_nev=kereszt_nev, kulcs_szam=kulcs_szam)
+        nyilvantartas.save()
 
+    return redirect('lapok.kezdolap')
