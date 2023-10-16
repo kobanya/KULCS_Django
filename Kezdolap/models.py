ce0a4cbe6cef58691bb9bf5b94a4a7802b0a0779
kobanya <nagy.bela.budapest@gmail.com>
2023-10-16 17:16:41 +0200

    A kulcsszám hiba javítása

--- Kezdolap/models.py
+++ Kezdolap/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Nyilvantartas(models.Model):
     vezetek_nev = models.CharField(max_length=30)
     kereszt_nev = models.CharField(max_length=30)
-    kulcs_szam = models.CharField(max_length=10)
+    kulcs_szam = models.CharField(max_length=15)
     datum = models.DateTimeField(auto_now_add=True)
     visszaadva = models.DateTimeField(null=True, blank=True)  # Új mező a visszaadás időpontjának
 
     def __str__(self):
-        return f"{self.vezetek_nev} {self.kereszt_nev}"
+        return f"{self.vezetek_nev}  {self.kereszt_nev} - {self.kulcs_szam}"
