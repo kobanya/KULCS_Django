5329fe87da01ef279f55f34ee2a1c14174fc4a0a
kobanya <nagy.bela.budapest@gmail.com>
2023-10-16 14:07:52 +0200

    Visszaadás gomb

--- Kezdolap/models.py
+++ Kezdolap/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Nyilvantartas(models.Model):
     vezetek_nev = models.CharField(max_length=30)
     kereszt_nev = models.CharField(max_length=30)
-    kulcs_szama = models.CharField(max_length=10)
+    kulcs_szam = models.CharField(max_length=10)
     datum = models.DateTimeField(auto_now_add=True)
+    visszaadva = models.DateTimeField(null=True, blank=True)  # Új mező a visszaadás időpontjának
 
     def __str__(self):
         return f"{self.vezetek_nev} {self.kereszt_nev}"
---
+++ _core/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from Kezdolap.models import Nyilvantartas
+
+def kezdolap(request):
+    nyilvantartas = Nyilvantartas.objects.all()
+    return render(request, 'index.html', {"nyilvantartas": nyilvantartas})
+
+def mentes(request):
+    if request.method == "POST":
+        vezetek_nev = request.POST.get('vezetek_nev')
+        kereszt_nev = request.POST.get('kereszt_nev')
+        kulcs_szam = request.POST.get('kulcs_szam')
+
+        # Ellenőrizze, hogy az összes szükséges adat rendelkezésre áll
+        if vezetek_nev and kereszt_nev and kulcs_szam:
+            # Hozzon létre egy új objektumot és mentsen az adatbázisba
+            Nyilvantartas.objects.create(vezetek_nev=vezetek_nev, kereszt_nev=kereszt_nev, kulcs_szam=kulcs_szam)
+
+    # Visszairányítás a főoldalra
+    return redirect('kezdolap')
+def visszaadva(request, nyilvantartas_id):
+    nyilvantartas = Nyilvantartas.objects.get(pk=nyilvantartas_id)
+    nyilvantartas.visszaadva = timezone.now()  # Beállítjuk az aktuális időpontra
+    nyilvantartas.save()
+    return redirect('kezdolap')  # Visszairányítjuk a főoldalra
