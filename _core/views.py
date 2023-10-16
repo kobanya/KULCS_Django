f653f3945b4c28f8a5573d56e9ad4f8db4eac6b2
kobanya <nagy.bela.budapest@gmail.com>
2023-10-16 18:36:07 +0200

    A kiadott kulcsok számának megjelenítése

--- _core/views.py
+++ _core/views.py
@@ -5,7 +5,12 @@ from Kezdolap.models import Nyilvantartas
 
 def kezdolap(request):
     nyilvantartas = Nyilvantartas.objects.all().order_by('visszaadva', '-datum')  # rendezés a vissza nem adaottak alapján
-    return render(request, 'index.html', {"nyilvantartas": nyilvantartas})
+    kiadott_kulcsok_szama = nyilvantartas.filter(visszaadva__isnull=True).count()
+
+    return render(request, 'index.html', {
+            "nyilvantartas": nyilvantartas,
+            "kiadott_kulcsok_szama": kiadott_kulcsok_szama,
+        })
 
 def mentes(request):
     if request.method == "POST":
